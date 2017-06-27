4d9922293ab42a51233e27dbf8c9b6c06ec12b7a
Viet Anh Doan <vtahdn@icloud.com>
2017-06-27 16:36:39 +0700

    Initial Commit

---
+++ 2048 Version 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2048 Version 1.0
+//
+//  Created by Viet Anh Doan on 6/27/17.
+//  Copyright © 2017 Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
